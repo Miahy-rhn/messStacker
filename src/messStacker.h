9c37bafb9bc415bf9f18e6c6ea7e58a4f4ff6ac9
Miahy <miahy.raharijaona@esiroi.re>
2026-03-27 07:20:47 +0000

    Fin de l'exercice 8

--- src/messStacker.h
+++ src/messStacker.h
@@ -1,9 +1,19 @@
 #ifndef MESS_STACKER_VERSION_H
 #define MESS_STACKER_VERSION_H
 
+#include <stdint.h>
+
 #define MESS_STACKER_VERSION "v0.0.01"
 #define SIZE_MAX_DATA 57
 
 const char* getMessStackerVersion(void);
+
+/* Structure principale pour un message */
+typedef struct Message{
+	uint8_t cmd;                    /* Commande : entier non signé 8 bits */
+	char data[SIZE_MAX_DATA];       /* Données : tableau d'octets */
+	uint8_t size;                   /* Taille utile des données */
+    uint8_t checksum;
+} Message;
 
 #endif // MESS_STACKER_VERSION_H
