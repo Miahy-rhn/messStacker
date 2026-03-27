cd30677d8b872e9c307fb5530d8d192bb62fb7e6
Miahy <miahy.raharijaona@esiroi.re>
2026-03-27 07:32:10 +0000

    Fin de l'exercice 10

--- src/messStacker.c
+++ src/messStacker.c
@@ -1,5 +1,17 @@
 #include "messStacker.h"
 
+
 const char* getMessStackerVersion(void) {
 	return MESS_STACKER_VERSION;
 }
+
+
+/* Constante de taille de la pile de messages */
+#define SIZE_STACK 30
+
+/* Variables globales internes */
+Message stackMess[SIZE_STACK];  /* File de messages circulaire */
+int lastPos = -1;               /* Position du dernier message posté */
+int curPos = 0;                 /* Position du message courant */
+int messCount = 0;              /* Nombre de messages dans la pile */
+
