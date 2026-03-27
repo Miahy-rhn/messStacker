0362d47bc33210a6f7799b26437fc7868abc724e
Miahy <miahy.raharijaona@esiroi.re>
2026-03-27 08:27:51 +0000

    version v0.1.00

--- src/messStacker.h
+++ src/messStacker.h
@@ -1,7 +1,7 @@
 #ifndef MESS_STACKER_VERSION_H
 #define MESS_STACKER_VERSION_H
 
-#define MESS_STACKER_VERSION "v0.0.01"
+#define MESS_STACKER_VERSION "v0.1.00"
 #define SIZE_MAX_DATA 57
 
 const char* getMessStackerVersion(void);
